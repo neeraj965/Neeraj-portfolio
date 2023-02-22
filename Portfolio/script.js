cd7d51ea8748a1565cc01cff2df7f4a569645c21
Neeraj pratap singh <125183729+neeraj965@users.noreply.github.com>
2023-02-22 23:52:03 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,11 @@
+// .about-text h5:hover{
+//     transform:scaleX(1.2);
+//     color:chartreuse;
+// }
+
+// var typed = new typed("#type", {
+//     strings: [ "Web-Developer", "Blogger"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// });
